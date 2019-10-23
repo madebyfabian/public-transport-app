5c4c7f7612374c9827ed1d246c6cbae8839ddeda
zap335213 <beer.fabian@mail.de>
2019-10-23 17:50:45 +0200

    test7

--- dist/precache-manifest.ee21482b2a58508655b0841dd151369e.js
+++ dist/precache-manifest.ee21482b2a58508655b0841dd151369e.js
@@ -24,24 +24,24 @@ self.__precacheManifest = [
     "url": "/js/journeys.6c8d3c66.js"
   },
   {
-    "revision": "f3ac50f54a6db010b64a6aa977b70a31",
-    "url": "/img/favicons/mstile-144x144.png"
+    "revision": "76185fc991a39c726cd0c71df81dbd39",
+    "url": "/img/favicons/mstile-310x150.png"
   },
   {
     "revision": "15d85d696b9fd4245bcb",
     "url": "/css/journeys.71d4c2bf.css"
   },
   {
-    "revision": "6e4fc62affa6e3e9504a453822b8416e",
-    "url": "/img/favicons/apple-touch-icon.png"
-  },
-  {
     "revision": "05bededbaf4c9a234aecec27e0c17dc2",
     "url": "/img/favicons/favicon-32x32.png"
   },
   {
-    "revision": "72912891148669bbc933",
-    "url": "/js/app.4caab8ca.js"
+    "revision": "6e4fc62affa6e3e9504a453822b8416e",
+    "url": "/img/favicons/apple-touch-icon.png"
+  },
+  {
+    "revision": "8231b274d5fcf231e6d7",
+    "url": "/js/app.377af9ae.js"
   },
   {
     "revision": "348c1291eeac59bf6a20a932156c95a7",
@@ -52,20 +52,20 @@ self.__precacheManifest = [
     "url": "/img/favicons/favicon-16x16.png"
   },
   {
-    "revision": "e6fd8edbcda173ce6847dfc4ae9f3abc",
+    "revision": "5fc38ac960851bbe0e3cd52b6c7f9094",
     "url": "/index.html"
   },
   {
-    "revision": "76185fc991a39c726cd0c71df81dbd39",
-    "url": "/img/favicons/mstile-310x150.png"
-  },
-  {
-    "revision": "3f8541b4b78044df66a150f2a77e47dc",
-    "url": "/img/favicons/android-chrome-512x512.png"
+    "revision": "f3ac50f54a6db010b64a6aa977b70a31",
+    "url": "/img/favicons/mstile-144x144.png"
   },
   {
     "revision": "0a75c460ca16b3a87457fc382890bca7",
     "url": "/img/favicons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "3f8541b4b78044df66a150f2a77e47dc",
+    "url": "/img/favicons/android-chrome-512x512.png"
   },
   {
     "revision": "bc98ad2596e2b347a2f894323b846e09",
@@ -76,7 +76,7 @@ self.__precacheManifest = [
     "url": "/css/journeyDetails.7cfaa5b6.css"
   },
   {
-    "revision": "72912891148669bbc933",
+    "revision": "8231b274d5fcf231e6d7",
     "url": "/css/app.f639a6bb.css"
   }
 ];
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fc0df82e210ee44e8c1b93df308cf563.js"
+  "/precache-manifest.ee21482b2a58508655b0841dd151369e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "client"});
