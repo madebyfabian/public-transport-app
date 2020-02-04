62a78f390c7ba0034d657c7e5a9ab34b0fd8515d
zap335213 <beer.fabian@mail.de>
2020-02-04 12:07:37 +0100

    Changed version to 2.2.1

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,8 @@
   import SVGIconProvider from '@/components/UI/SVGIconProvider.vue'
   import TabBar from '@/components/UI/TabBar.vue'
 
+  import { version as CURR_VERSION } from '../package.json'
+
   export default {
     name: 'App',
     components: {
@@ -46,6 +48,10 @@
 
       // Call when viewport size updated
       window.visualViewport.addEventListener('resize', viewportHandler)
+
+      // if curr version in URL is not the current package.json version
+      if (!this.$route.fullPath.startsWith(`/${CURR_VERSION}`)) 
+        this.$router.push('/')
     },
 
     methods: {
